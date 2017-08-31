e5b8ddb5654bccfbf0d71ed64f5e35a7ba86781d
Senthil G <gdr.senthil@gmail.com>
2017-08-31 18:02:59 +0530

    CertificationApp Spring boot created

---
+++ src/main/java/in/spinsoft/certificationapp/CertificationappApplication.java
@@ -0,0 +1,12 @@
+package in.spinsoft.certificationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationappApplication.class, args);
+	}
+}
